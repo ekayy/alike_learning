29cb2a44c32e1084dd4b45075c1041161ad04dae
Eric K Wu <ekuan.wu@gmail.com>
2020-08-18 13:43:01 -0700

    Phone verification UI with pin_code_fields package

--- lib/src/auth/auth_verify.dart
+++ lib/src/auth/auth_verify.dart
@@ -1,5 +1,7 @@
-import 'package:alike_learning/src/core/button.dart';
+import 'package:alike_learning/src/auth/auth_create_account.dart';
 import 'package:flutter/material.dart';
+import 'package:alike_learning/src/core/button.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 class AuthVerify extends StatefulWidget {
   @override
@@ -7,44 +9,143 @@ class AuthVerify extends StatefulWidget {
 }
 
 class _AuthVerifyState extends State<AuthVerify> {
-  final TextEditingController _controller = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _pinController = TextEditingController();
+  bool showVerification = false;
+  bool hasError = false;
+  String currentText = "";
+
+  Widget phoneInput() {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: TextFormField(
+            textAlign: TextAlign.right,
+            decoration: InputDecoration(
+              hintText: 'US +1',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: TextFormField(
+            controller: _phoneController,
+            decoration: InputDecoration(
+              hintText: '(510) 555-5555)',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget pinInput() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+      decoration: BoxDecoration(
+          border: Border.all(color: Color.fromRGBO(151, 151, 151, 1.0))),
+      child: Container(
+        width: 280,
+        child: PinCodeTextField(
+          appContext: context,
+          backgroundColor: Colors.transparent,
+          pastedTextStyle: TextStyle(
+            // color: Colors.green.shade600,
+            fontWeight: FontWeight.bold,
+          ),
+          length: 4,
+          obsecureText: false,
+          animationType: AnimationType.fade,
+          validator: (v) {
+            if (v.length < 3) {
+              return "I'm from validator";
+            } else {
+              return null;
+            }
+          },
+          pinTheme: PinTheme(
+            shape: PinCodeFieldShape.circle,
+            borderWidth: 0,
+            borderRadius: BorderRadius.circular(5),
+            fieldHeight: 40,
+            fieldWidth: 40,
+            activeFillColor: Colors.grey[200],
+            inactiveFillColor: Colors.grey[200],
+            selectedFillColor: Colors.grey[200],
+            inactiveColor: Colors.transparent,
+          ),
+          animationDuration: Duration(milliseconds: 300),
+          enableActiveFill: true,
+          // errorAnimationController: errorController,
+          controller: _pinController,
+          onCompleted: (v) {
+            print("Completed");
+          },
+          onTap: () {
+            print("Pressed");
+          },
+          onChanged: (value) {
+            print(value);
+            setState(() {
+              currentText = value;
+            });
+          },
+          beforeTextPaste: (text) {
+            print("Allowing to paste $text");
+            //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+            //but you can show anything you want here, like your pop up saying wrong paste format or etc
+            return true;
+          },
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        widthFactor: 280,
+        // widthFactor: 280,
         child: Column(
           children: <Widget>[
             SizedBox(height: 40),
             Text('Let\'s get started'),
             Text('enter your phone number'),
             SizedBox(height: 60),
-            Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      labelText: '(510) 555-5555)',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            (!showVerification ? (phoneInput()) : pinInput()),
             SizedBox(height: 60),
-            Button(
-              text: 'Send text',
-              onPress: () {},
-            ),
+            (!showVerification
+                ? (Button(
+                    text: (!showVerification ? 'Send text' : 'Confirm'),
+                    onPress: () {
+                      setState(() => showVerification = true);
+                    }))
+                : (Button(
+                    text: 'Confirm',
+                    onPress: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AuthCreateAccount()),
+                      );
+                    }))),
             SizedBox(height: 20),
-            Text('Receive phone call instead'),
+            (!showVerification
+                ? Text('Receive phone call instead')
+                : Text('Try another option')),
           ],
         ),
       ),
     );
   }
+
+  // @override
+  // void dispose() {
+  //   _controller?.dispose();
+  //   super.dispose();
+  // }
 }
