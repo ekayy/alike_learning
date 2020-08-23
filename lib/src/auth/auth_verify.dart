62e3b5064dc8a83ebcf5ecc41a279296b9ef4039
Eric K Wu <ekuan.wu@gmail.com>
2020-08-23 10:37:28 -0700

    Add ios-style picker for handling country code

--- lib/src/auth/auth_verify.dart
+++ lib/src/auth/auth_verify.dart
@@ -1,7 +1,8 @@
 import 'package:alike_learning/src/auth/auth_create_account.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:alike_learning/src/core/button.dart';
-import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:pin_code_text_field/pin_code_text_field.dart';
 
 class AuthVerify extends StatefulWidget {
   @override
@@ -9,96 +10,118 @@ class AuthVerify extends StatefulWidget {
 }
 
 class _AuthVerifyState extends State<AuthVerify> {
+  final _phoneIsoController = TextEditingController(text: 'US +1');
   final _phoneController = TextEditingController();
   final _pinController = TextEditingController();
+
+  bool showPicker = false;
   bool showVerification = false;
   bool hasError = false;
-  String currentText = "";
+
+  List<String> countryCodes = ['US +1', 'UK +44', 'AUS +61'];
 
   Widget phoneInput() {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: TextFormField(
-            textAlign: TextAlign.right,
-            decoration: InputDecoration(
-              hintText: 'US +1',
-              border: OutlineInputBorder(),
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Color.fromRGBO(151, 151, 151, 1.0))),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              controller: _phoneIsoController,
+              readOnly: true,
+              showCursor: false,
+              textAlign: TextAlign.right,
+              decoration: InputDecoration(
+                hintText: 'US +1',
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                contentPadding:
+                    EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+              ),
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+              onTap: () {
+                setState(() => {showPicker = true});
+              },
             ),
           ),
-        ),
-        Expanded(
-          flex: 2,
-          child: TextFormField(
-            controller: _phoneController,
-            decoration: InputDecoration(
-              hintText: '(510) 555-5555)',
-              border: OutlineInputBorder(),
-            ),
+          Container(
+            width: 1,
+            height: 60,
+            color: Color.fromRGBO(151, 151, 151, 1.0),
+            // constraints: BoxConstraints.expand(),
           ),
-        ),
-      ],
+          // ),
+          Expanded(
+            flex: 2,
+            child: TextFormField(
+                keyboardType: TextInputType.number,
+                controller: _phoneController,
+                decoration: InputDecoration(
+                  hintText: '(510) 555-5555',
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  contentPadding:
+                      EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+                ),
+                style: TextStyle(
+                  fontSize: 18.0,
+                ),
+                onTap: () {
+                  setState(() => {showPicker = false});
+                }),
+          ),
+        ],
+      ),
     );
   }
 
   Widget pinInput() {
     return Container(
       width: MediaQuery.of(context).size.width,
-      padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+      height: 64,
       decoration: BoxDecoration(
           border: Border.all(color: Color.fromRGBO(151, 151, 151, 1.0))),
-      child: Container(
-        width: 280,
+      child: Center(
         child: PinCodeTextField(
-          appContext: context,
-          backgroundColor: Colors.transparent,
-          pastedTextStyle: TextStyle(
-            // color: Colors.green.shade600,
-            fontWeight: FontWeight.bold,
-          ),
-          length: 4,
-          obsecureText: false,
-          animationType: AnimationType.fade,
-          validator: (v) {
-            if (v.length < 3) {
-              return "I'm from validator";
-            } else {
-              return null;
-            }
-          },
-          pinTheme: PinTheme(
-            shape: PinCodeFieldShape.circle,
-            borderWidth: 0,
-            borderRadius: BorderRadius.circular(5),
-            fieldHeight: 40,
-            fieldWidth: 40,
-            activeFillColor: Colors.grey[200],
-            inactiveFillColor: Colors.grey[200],
-            selectedFillColor: Colors.grey[200],
-            inactiveColor: Colors.transparent,
-          ),
-          animationDuration: Duration(milliseconds: 300),
-          enableActiveFill: true,
-          // errorAnimationController: errorController,
+          autofocus: true,
           controller: _pinController,
-          onCompleted: (v) {
-            print("Completed");
-          },
-          onTap: () {
-            print("Pressed");
-          },
-          onChanged: (value) {
-            print(value);
+          highlight: true,
+          pinBoxColor: Colors.grey[200],
+          highlightPinBoxColor: Colors.grey[300],
+          highlightColor: Colors.transparent,
+          defaultBorderColor: Colors.transparent,
+          hasTextBorderColor: Colors.transparent,
+          maxLength: 4,
+          hasError: hasError,
+          onTextChanged: (text) {
             setState(() {
-              currentText = value;
+              hasError = false;
             });
           },
-          beforeTextPaste: (text) {
-            print("Allowing to paste $text");
-            //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
-            //but you can show anything you want here, like your pop up saying wrong paste format or etc
-            return true;
+          onDone: (text) {
+            print("DONE $text");
+            print("DONE CONTROLLER ${_pinController.text}");
           },
+          pinBoxWidth: 40,
+          pinBoxHeight: 40,
+          pinBoxRadius: 40,
+          wrapAlignment: WrapAlignment.spaceAround,
+          pinBoxDecoration: ProvidedPinBoxDecoration.defaultPinBoxDecoration,
+          pinTextStyle: TextStyle(fontSize: 18.0),
+          pinTextAnimatedSwitcherTransition:
+              ProvidedPinBoxTextAnimation.scalingTransition,
+          pinTextAnimatedSwitcherDuration: Duration(milliseconds: 300),
+          keyboardType: TextInputType.number,
         ),
       ),
     );
@@ -109,43 +132,67 @@ class _AuthVerifyState extends State<AuthVerify> {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        // widthFactor: 280,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            SizedBox(height: 40),
-            Text('Let\'s get started'),
-            Text('enter your phone number'),
-            SizedBox(height: 60),
-            (!showVerification ? (phoneInput()) : pinInput()),
-            SizedBox(height: 60),
-            (!showVerification
-                ? (Button(
-                    text: (!showVerification ? 'Send text' : 'Confirm'),
-                    onPress: () {
-                      setState(() => showVerification = true);
-                    }))
-                : (Button(
-                    text: 'Confirm',
-                    onPress: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => AuthCreateAccount()),
-                      );
-                    }))),
-            SizedBox(height: 20),
-            (!showVerification
-                ? Text('Receive phone call instead')
-                : Text('Try another option')),
+            Column(
+              children: [
+                SizedBox(height: 40),
+                Text('Let\'s get started'),
+                Text('enter your phone number'),
+                SizedBox(height: 60),
+                (!showVerification ? (phoneInput()) : pinInput()),
+                SizedBox(height: 60),
+                (!showVerification
+                    ? (Button(
+                        text: (!showVerification ? 'Send text' : 'Confirm'),
+                        onPress: () {
+                          setState(() => showVerification = true);
+                        }))
+                    : (Button(
+                        text: 'Confirm',
+                        onPress: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => AuthCreateAccount()),
+                          );
+                        }))),
+                SizedBox(height: 20),
+                (!showVerification
+                    ? Text('Receive phone call instead')
+                    : Text('Try another option')),
+              ],
+            ),
+            if (showPicker)
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  height: 200,
+                  child: CupertinoPicker(
+                    children: <Widget>[
+                      ...countryCodes.map(
+                        (String code) {
+                          return Center(child: Text(code));
+                        },
+                      )
+                    ],
+                    itemExtent: 80,
+                    onSelectedItemChanged: (int index) {
+                      _phoneIsoController.text = countryCodes[index];
+                    },
+                  ),
+                ),
+              ),
           ],
         ),
       ),
     );
   }
 
-  // @override
-  // void dispose() {
-  //   _controller?.dispose();
-  //   super.dispose();
-  // }
+  @override
+  void dispose() {
+    _pinController?.dispose();
+    super.dispose();
+  }
 }
