cc016a06ae03f651a76de90b36a2c3a161a4ce95
Eric K Wu <ekuan.wu@gmail.com>
2020-08-23 13:59:46 -0700

    Add svg library to handle svgs +  alike logo to splash screen

--- lib/src/auth/sign_up_splash_screen.dart
+++ lib/src/auth/sign_up_splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:alike_learning/src/common/button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SignUpSplashScreen extends StatelessWidget {
   @override
@@ -12,10 +13,8 @@ class SignUpSplashScreen extends StatelessWidget {
           children: <Widget>[
             Column(
               children: <Widget>[
-                Icon(
-                  CupertinoIcons.heart,
-                ),
-                Text('alike'),
+                SvgPicture.asset('assets/icons/logo.svg',
+                    semanticsLabel: 'Alike logo'),
                 SizedBox(height: 10),
                 Text('Tell your story and find love',
                     style: TextStyle(fontSize: 20)),
