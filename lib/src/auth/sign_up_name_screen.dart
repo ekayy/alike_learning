f29829169a52427bd53041aea09a8f19942974e4
Eric K Wu <ekuan.wu@gmail.com>
2020-08-28 10:37:24 -0700

    Validation for name field

--- lib/src/auth/sign_up_name_screen.dart
+++ lib/src/auth/sign_up_name_screen.dart
@@ -1,35 +1,71 @@
 import 'package:alike_learning/src/common/button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SignUpNameScreen extends StatelessWidget {
+  final _nameFormKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Column(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 50),
           children: <Widget>[
-            const SizedBox(height: 40),
-            Text('Your name', style: TextStyle(fontSize: 25)),
-            const SizedBox(height: 40),
+            Text(
+              'Your name',
+              style: const TextStyle(fontSize: 28, color: Color(0xFF1B1B1B)),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
             Container(
+              alignment: Alignment.topCenter,
+              height: 72,
               margin: EdgeInsets.symmetric(vertical: 20),
-              padding: EdgeInsets.symmetric(horizontal: 50),
-              child: TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Name',
+              child: Form(
+                key: _nameFormKey,
+                child: TextFormField(
+                  textCapitalization: TextCapitalization.words,
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter(50),
+                    FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]")),
+                  ],
+                  validator: (String value) {
+                    if (value.isEmpty) {
+                      return 'Name is required.';
+                    }
+                    if (value.length < 3) {
+                      return 'Name is too short.';
+                    }
+                    return null;
+                  },
+                  style: TextStyle(fontSize: 18),
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+                    focusedBorder: const UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: const Color(0xFF000000),
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
-            const SizedBox(height: 10),
-            Text('This cannot be changed later',
-                style: TextStyle(fontWeight: FontWeight.w200)),
-            const SizedBox(height: 30),
+            Text(
+              'This cannot be changed later',
+              style: TextStyle(fontWeight: FontWeight.w200),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 35),
             Button(
               text: 'Next',
               onPress: () {
-                Navigator.pushNamed(context, '/signUpBirthday');
+                if (_nameFormKey.currentState.validate()) {
+                  Navigator.pushNamed(context, '/signUpBirthday');
+                }
               },
             ),
           ],
